781f4bca8d50de566ab8615ff7402a55e31c2d73
Anikesh Sinha <anikeshsinha041198@gmail.com>
2021-01-22 14:07:54 +0530

    add script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const File = require("./model/file");
+const fs = require("fs");
+const connectDB = require("./config/db");
+
+connectDB();
+
+async function deleteUploadedFile(){
+    const pastDate = new Date(Date.now() - (24 * 60 * 60 * 1000));
+    const files = await File.find({ createdAt: {$lt: pastDate}});
+    if(files.length){
+        for(const file of files){
+           try
+           {
+            fs.unlinkSync(file.path);
+            await file.remove();
+            console.log(`successfully deleted ${file.filename}`);
+        }catch(err){
+            console.log(`Error while deleting file ${err}`);
+        } 
+        }
+        console.log("Job done");
+    }
+}
+
+deleteUploadedFile().then(process.exit);
